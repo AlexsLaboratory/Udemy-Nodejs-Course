07387f5e8c96f54a4b287c4acc90f79cf5a09d92
Alex Lowe <loweman64@outlook.com>
2021-07-25 23:44:15 -0400

    feat: Request & Response Headers

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head>");
+  res.write("<title>My First Nodejs Site</title>");
+  res.write("</head>");
+  res.write("<body>");
+  res.write("<h1>Hello from my Nodejs Server!</h1>");
+  res.write("</body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000)
